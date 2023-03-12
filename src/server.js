73773b2eb3c48079931a4a16a549ef2164ed5bf2
Ayo Onamusi <onamusia@gmail.com>
2023-03-12 00:33:45 +0000

    comment out some server.register

--- src/server.js
+++ src/server.js
@@ -47,7 +47,7 @@ async function init(){
   });
   await server.register(Vision);
   await server.register(Cookie);
-  await server.register(Inert);
+ // await server.register(Inert);
   await server.register(jwt);
   await server.register([
     Inert,
